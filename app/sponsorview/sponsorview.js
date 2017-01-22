abeb96134b765e8acb2b13c876cc9579bd997632
Aditya Chitnis <Aditya Chitnis>
2017-01-21 21:29:17 -0500

    added angularjs to change sponsor page dynamically

--- app/sponsorview/sponsorview.js
+++ app/sponsorview/sponsorview.js
@@ -9,6 +9,28 @@ angular.module('myApp.sponsorview', ['ngRoute'])
   });
 }])
 
-.controller('SponsorController', [function() {
-
+.controller('SponsorController', ['$scope', function($scope) {
+    var Company1={
+      CompanyImageURL: "../images/githubStockPic.png",
+      CompanyName: "Company 1",
+      AboutUs: "We are company unoooo",
+      APIs: "We got this nice nice one API",
+      PrizesContests: "We got one prize boiiii"
+    };
+    var Company2={
+      CompanyImageURL: "../images/githubStockPic1.png",
+      CompanyName: "Company 2",
+      AboutUs: "We are company toooooo",
+      APIs: "Tu",
+      PrizesContests: "Only got tuu"
+    };
+    var Company3={
+      CompanyImageURL: "../images/githubStockPic2.png",
+      CompanyName: "Company 3",
+      AboutUs: "We are company thrrrreeee",
+      APIs: "Tree",
+      PrizesContests: "Only got that third one from the top"
+    };
+    $scope.companyArray=[Company1,Company2,Company3];
+    $scope.companyNum = 0;
 }]);
