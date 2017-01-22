d74c9661b54ed6c9054f0b3a09dd5e9f033b28b8
Aditya Chitnis <Aditya Chitnis>
2017-01-22 07:05:55 -0500

    added swag section and logout in tr corner

--- app/sponsorview/sponsorview.js
+++ app/sponsorview/sponsorview.js
@@ -15,21 +15,24 @@ angular.module('myApp.sponsorview', ['ngRoute'])
       CompanyName: "Company 1",
       AboutUs: "We are company unoooo",
       APIs: "We got this nice nice one API",
-      PrizesContests: "We got one prize boiiii"
+      PrizesContests: "We got one prize boiiii",
+      Swag: "Capital 1 T-Shirt and coffee cereal"
     };
     var Company2={
       CompanyImageURL: "../images/githubStockPic1.png",
       CompanyName: "Company 2",
       AboutUs: "We are company toooooo",
       APIs: "Tu",
-      PrizesContests: "Only got tuu"
+      PrizesContests: "Only got tuu",
+      Swag: "T-Shirt and Pens"
     };
     var Company3={
       CompanyImageURL: "../images/githubStockPic2.png",
       CompanyName: "Company 3",
       AboutUs: "We are company thrrrreeee",
       APIs: "Tree",
-      PrizesContests: "Only got that third one from the top"
+      PrizesContests: "Only got that third one from the top",
+      Swag: "Water bottles and discs"
     };
     $scope.companyArray=[Company1,Company2,Company3];
     $scope.companyNum = 0;
